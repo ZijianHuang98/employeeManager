816798f4ae06c9512dd591ecfc2b42b0ad8e92b4
Zijian Huang <jessehuang05@yahoo.com>
2022-03-09 10:14:48 -0600

    fixed the no displaying bug

--- employeeManagerapp/src/app/app.component.ts
+++ employeeManagerapp/src/app/app.component.ts
@@ -1,22 +1,35 @@
 import { HttpErrorResponse } from '@angular/common/http';
+import { computeDecimalDigest } from '@angular/compiler/src/i18n/digest';
 import { Component, OnInit } from '@angular/core';
-import {Employee} from './employee'
+import { Employee } from './employee'
 import { EmployeeService } from './employee.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'employeeManagerapp'
   employees: Employee[] = []
-  constructor(private employeeService: EmployeeService){}
+  // demos:Employee[] = [{id:123,
+  //   name:"zijian",
+  //   phone:'911',
+  //   imageUrl:' www.gg.com',
+  //   employeeCode:'code',
+  //   jobTitle:'title',
+  //   email:'jessehuang05@yahoo.com'}]
+  constructor(private employeeService: EmployeeService) { }
   ngOnInit(): void {
-    this.getEmployees
+    this.getEmployees()
+    //console.log('ngOnInit called')
   }
-  public getEmployees():void{
+  public getEmployees(): void {
+    //console.log("getEmployees called")
     this.employeeService.getEmployees().subscribe({
-      next:(res) => {this.employees = res}
+      next: (res) => {
+        this.employees = res
+        //console.log('subscribe successfully!!')
+      }
     }
       // (res:Employee[])=>{
       //   this.employees = res
